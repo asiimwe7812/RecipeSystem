64259f1d2be78d23d06523e218c9fb9492f69665
asiimwe7812 <asiimwe7610@gmail.com>
2022-08-04 15:53:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Recipe from './recipe';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./recipe";
 
 const App = () => {
   const APP_ID = "a2c273b1";
   const APP_KEY = "20305f39b43b339dfb229355cf67053e	";
   const [recipes, setRecipe] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
-
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-
   }, [query]);
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
     setRecipe(data.hits);
-
-  }
-  const getSearch = e => {
+  };
+  const getSearch = (e) => {
     e.preventDefault();
     setQuery(search);
-    setSearch('');
-  }
+    setSearch("");
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Ivan RecipeApp</h1>
-  
 
-      <form onSubmit={getSearch} className='search-form'>
-        < input className='search-bar' value={search} type="text" onChange={(e) => setSearch(e.target.value)} />
-        <button className='search-button' type='submit'>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          value={search}
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="search-button" type="submit">
           search
         </button>
-
-
       </form>
 
-      <div className='recipe'>
-        {recipes.map(recipe => (<Recipe key={recipe.recipe.label} Title={recipe.recipe.label} carolies={recipe.recipe.carolies}
-          image={recipe.recipe.image} ingredients={recipe.recipe.ingredients} />))}
+      <div className="recipe">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            Title={recipe.recipe.label}
+            carolies={recipe.recipe.carolies}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
       </div>
-
     </div>
-
   );
-
-
-
 };
-
-
 
 export default App;
